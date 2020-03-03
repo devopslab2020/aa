ab99d284ca7e773bd65f40db650d5f8c78d54125
beit2020# <devopslab2020@gmail.com>
2020-03-03 11:50:34 +0530

    first commit

---
+++ java/less.java
@@ -0,0 +1,15 @@
+public class less
+{
+public static void main(String args[])
+{
+int a=44,b=33;
+if(a>b)
+{
+System.out.println("a is greater");
+}
+else
+{
+System.out.println("b is greater");
+}
+}
+}
---
+++ java/multiply.java
@@ -0,0 +1,9 @@
+public class multiply
+{
+public static void main(String args[])
+{
+int a=40,b=10;
+int c=a*b;
+System.out.println(" multiplication of two numbers is "+c);
+}
+}
